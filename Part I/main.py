da789ae50a0e2ba1dbd3273a7a0ed5db660b21e8
coolmanjingwu <56213599+coolmanjingwu@users.noreply.github.com>
2020-03-11 15:56:50 -0700

    Update main.py

--- Part I/main.py
+++ Part I/main.py
@@ -19,30 +19,29 @@ from sklearn import neighbors
 def load_process_data():
     """load two datasets from s3"""
     file_name = "s3://stats404-project-jingwu/properties_2016.csv"
-    prop = pd.read_csv(filepath_or_buffer=file_name,
-                       encoding='latin-1')
+    prop2016 = pd.read_csv(filepath_or_buffer=file_name,
+                           encoding='latin-1')
 
     file_name = "s3://stats404-project-jingwu/train_2016_v2.csv"
     train = pd.read_csv(filepath_or_buffer=file_name,
                         encoding='latin-1')
 
     """feature engineering/data cleaning/merging"""
-    X = prop[['parcelid', 'taxamount', 'yearbuilt', 'bathroomcnt', 'bedroomcnt',
-              'calculatedfinishedsquarefeet']]
-    X = X.dropna()
+    # EDA ideas came from 
+    # https://www.kaggle.com/nathantheshark/exploration-of-career-length
+    X = prop2016[['parcelid', 'taxamount', 'yearbuilt', 'bathroomcnt', 'bedroomcnt',
+                  'calculatedfinishedsquarefeet']] 
+    # drop missing and 0 values
+    X = X[(X.bedroomcnt != 0) & (X.bathroomcnt != 0) &
+          (X.calculatedfinishedsquarefeet != 0)]
     # drop na
-    X = X[(X.bathroomcnt > 0) & (X.bedroomcnt > 0) &
-          (X.calculatedfinishedsquarefeet > 0)]
-    # drop missing values
-    X = pd.merge(X, train, on='parcelid')  # Merge X and train
-    X.sort_values(by=['parcelid', 'transactiondate'], inplace=True)
+    X = X.dropna()
+    # Merge X with train on parcelid
+    X = pd.merge(X, train, on='parcelid')  
     X['transactiondate'] = pd.to_datetime(X['transactiondate'], errors='coerce')
-    X['transaction_yearmonth'] = 100 * X['transactiondate'].dt.year + \
+    # transform transaction yearmonth into numeric values, easier to work with.
+    X['transaction_ym'] = 100 * X['transactiondate'].dt.year + \
                                  X['transactiondate'].dt.month
-    min_transaction_yearmonth = X.transaction_yearmonth.min()
-    X['transaction_yearmonth_i'] = X.transaction_yearmonth - \
-                                   min_transaction_yearmonth
-
     return X
 
 
@@ -50,7 +49,7 @@ def split_data_X():
     """split data for model fitting."""
     X_final = load_process_data()[['bathroomcnt', 'bedroomcnt', 'taxamount',
                                    'yearbuilt', 'calculatedfinishedsquarefeet',
-                                   'transaction_yearmonth_i']]
+                                   'transaction_ym']]
     return X_final
 
 
@@ -84,12 +83,12 @@ def user_inputs():
     yearbuilt = input("Please enter the year the house is built as 'yyyy': ")
     calculatedfinishedsquarefeet = \
         input("Please enter the calculatedfinishedsquarefeet: ")
-    transaction_yearmonth_i = \
+    transaction_ym = \
         input("Please enter the transaction_yearmonth as 'yyyymm': ")
     # creating lists based on user input
     user_inputs = np.array(
         [[bathroomcnt, bedroomcnt, taxamount, yearbuilt,
-          calculatedfinishedsquarefeet, transaction_yearmonth_i]])
+          calculatedfinishedsquarefeet, transaction_ym]])
     return user_inputs.astype(np.float64)
 
 LOGGER.info("---Initializing model...")
