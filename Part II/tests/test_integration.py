886deeb9d4237ce6c2d3cd5399e3fa72e29adc23
coolmanjingwu <56213599+coolmanjingwu@users.noreply.github.com>
2020-03-11 14:01:42 -0700

    Update test_integration.py

--- Part II/tests/test_integration.py
+++ Part II/tests/test_integration.py
@@ -11,7 +11,7 @@ import s3fs
 from year_adjustment.limit_on_years import built_year, yearmonth_transaction
 
 def initialize_model():
-    """this function will load and initialize the model"""
+    """this function will load from aws s3 and initialize the model"""
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('stats404-project-jingwu')
     bucket.download_file('knn_zillow.joblib', 'knn_zillow.joblib')
@@ -36,9 +36,7 @@ def test_year_adjustment_integration():
     user_inputs_final = user_inputs.astype(np.float64)
     output_log_error = knn_dict.predict(user_inputs_final)
     assert math.fabs(output_log_error - expected_output_log_error) < 0.001, \
-    """Should show that the price is reduced by less than 25%, after taxes
-       are applied to the discounted price.
-    """
+   
                                                                        
                                                                                
                                                                               
