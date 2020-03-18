2cce3c43f9ef65ff7302d9cedaa44c1a91b4727d
coolmanjingwu <56213599+coolmanjingwu@users.noreply.github.com>
2020-03-17 22:14:57 -0700

    Add files via upload

--- pytest/tests/test_integration.py
+++ pytest/tests/test_integration.py
@@ -1,19 +1,19 @@
-"""Module for integration testing of user inputs related functions.
+"""Module for integration testing of 'adjust_price' functions.
 """
 import math
 import pandas as pd
 import numpy as np
 
 
-from year_adjustment.limit_on_years import built_year, yearmonth_transaction
-from year_adjustment.limit_on_years import inputs_not_nan
+from year_adjustment.limit_on_years import built_year, yearmonth_transaction, \
+    inputs_not_nan
+# from year_adjustment.limit_on_years import inputs_not_nan
 
 
 
 def test_input_integration():
-    """Integration test to check that the model is taking the valid inputs.
-       First a test on year inputs, then test whether there's a nan value in
-       the input array.
+    """Integration test to check that we can get the final price of an item
+       in case the item is discounted, before taxes are applied.
     """
     expected_inputs = np.array([[4, 5, 13245, 1962, 4000, 200909]])
     yearbuilt = built_year(1962)
@@ -25,7 +25,8 @@ def test_input_integration():
     inputs = np.array([[4, 5, 13245, yearbuilt, 4000, transaction_yearmonth_i]])
     actual_inputs = inputs_not_nan(inputs)
     assert np.array_equal(expected_inputs, actual_inputs), \
-    """Should show that the user inputs are valid this time.
+    """Should show that the price is reduced by less than 25%, after taxes
+       are applied to the discounted price.
     """
                                                                        
                                                                                
